27e80301a248b1833ada0bdcf16ad291d8560f02
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-04-15 00:43:23 +0600

    added updationisr

--- src/components/RevewItem/RevewItem.jsx
+++ src/components/RevewItem/RevewItem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './RevewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const RevewItem = ({product}) => {
-    console.log(product);
+const RevewItem = ({ product }) => {
+    const { id, img, name, price, quantity, shipping } = product;
     return (
         <div className='revew-item'>
-            <h2>revewItem</h2>
+            <div className='showOrderInfo'>
+                <img src={img} alt="" />
+                < div className='revew-details'>
+                    <p>{name}</p>
+                    <p>Price: <span className='color'>${price}</span></p>
+                    <p>Order quantity: <span className='order-quantity'>{quantity}</span></p>
+
+                </div>
+            </div>
+            <button className='delete-button'><FontAwesomeIcon icon={faTrashAlt} /></button>
         </div>
     );
 };
